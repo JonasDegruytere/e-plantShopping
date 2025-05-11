1284a197b5513ea0e69e171233c73aa1609657ca
JonasDegruytere <gargantuanimpact@gmail.com>
2025-05-11 10:05:38 -0400

    test

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -274,8 +274,20 @@ function ProductList({ onHomeClick }) {
             </div>
             {!showCart ? (
                 <div className="product-grid">
-
-
+                    <br></br>
+                    {plantsArray.map((item)=><div className='mainCategoryDiv'> <h1>{item.category}</h1> 
+                    <div className="product-list">
+                    {item.plants.map((plant)=>
+                        <div className='product-card'>
+                            <img className='product-image' src={plant.image} alt={plant.name} />
+                            <h2>{plant.name}</h2>
+                            <p>{plant.description}</p>
+                            <p>{plant.cost}</p>
+                            </div>)}
+                    </div>
+                    </div>)}
+        
+        
                 </div>
             ) : (
                 <CartItem onContinueShopping={handleContinueShopping} />
